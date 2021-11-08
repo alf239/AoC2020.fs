1c668e3de8b26fb55bd2dd29350ce9a8efffe281
Alexey Filippov <alexey@filippov.uk>
2021-11-08 18:38:51 +0000

    Day 1 in F#

--- Program.fs
+++ Program.fs
@@ -1,13 +1,41 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
 open System
+open System.Collections.Generic
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+let readLines filePath = System.IO.File.ReadLines(filePath)
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+    let year = 2020
+    let pairFor x = year - x
+    let lines = readLines "C:\\Users\\alexe\\Learning\\AoC2020.fs\\inputs\\day_01.txt"
+    let ints = lines |> Seq.map int
+    let found = (        
+        ints
+        |> Seq.scan
+            (fun state i ->
+                let (seen: int Set), _ = state
+                let valueIfPaired = if seen.Contains(pairFor i) then Some(i) else None
+                seen.Add(i), valueIfPaired)
+            (Set.empty, None)
+        |> Seq.map (fun (_, valueIfPaired) -> valueIfPaired)
+        |> Seq.find Option.isSome
+    )
+    let number = found.Value
+    let other = pairFor number
+    printfn "1. %d * %d = %d" number other (number * other)
+
+    let xs = ints |> Seq.toArray
+    let n = Array.length xs
+    for i in 1 .. n-3 do
+        let a = xs.[i]
+        let index = new Dictionary<int, int>() // sum -> product
+        for j in 0 .. i-1 do
+            let b = xs.[j]
+            // Go directly to target c
+            index.Add(year - a - b, a * b)
+        for j in i+1 .. n-1 do
+            let c = xs.[j]
+            let (found, product) = index.TryGetValue(c)
+            if found then printfn "2. %d" (c * product)
+
+    0
